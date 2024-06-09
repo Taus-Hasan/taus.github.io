64a2d369dbb140b598c43ee056bb7ef668e5cce9
Taus-Hasan <taushasan.earth@gmail.com>
2024-06-09 23:08:00 +0600

    js upt
    toast

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+alert("Have great day ...");
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
